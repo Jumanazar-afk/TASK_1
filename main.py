449ec83f178995a625b1a587aca1534ddf6300d2
Azambek Jurayev <JurayevA@corp.itstep>
2023-07-05 20:16:42 +0500

    main

--- main.py
+++ main.py
@@ -6,3 +6,36 @@ class People():
         self.city = city
         self.country = country
         self.adress = adress
+
+    def nsf(self):
+        return self.NSF()
+
+    def born(self):
+        return self.born()
+
+    def phone(self):
+        return self.phone()
+
+    def city(self):
+        return self.city()
+
+    def country(self):
+        return self.country()
+
+    def adress(self):
+        return self.adress()
+
+
+a = input(People.nsf)
+b = input(People.born)
+c = input(People.phone)
+d = input(People.city)
+e = input(People.country)
+f = input(People.adress)
+
+print(a,"My name is:",
+      b,"i was born:",
+      c,"my phone = ",
+      d,"my city:",
+      e,"my country:",
+      f,"my adress:")
