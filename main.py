b40fd940cd6852b27f880081cc83e6cddc20b096
Azambek Jurayev <JurayevA@corp.itstep>
2023-07-05 19:59:03 +0500

    task-1

---
+++ main.py
@@ -0,0 +1,8 @@
+class People():
+    def __init__(self, NSF, born, phone, city, country, adress):
+        self.NSF = NSF
+        self.born = born
+        self.phone = phone
+        self.city = city
+        self.country = country
+        self.adress = adress
